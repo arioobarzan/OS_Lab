1d5a920e9b8068ca672a7be49057e00c47c56372
Arioo Barzan <arioobarzan@hotmail.com>
2024-03-10 07:39:43 +0330

    upload

---
+++ OS_Sat/OS_Sat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+namespace OS_Sat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("mspaint");
+            Process[] prList = Process.GetProcesses();
+            foreach (Process p in prList)
+            {
+                Console.WriteLine(p.Id + "\t" + p.ProcessName);
+            }
+            int pid = int.Parse(Console.ReadLine());
+            foreach (Process p in prList)
+            {
+                if (p.Id == pid)
+                    p.Kill();
+            }
+            Console.ReadKey();
+        }
+    }
+}
