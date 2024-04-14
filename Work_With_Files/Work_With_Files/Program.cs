99525349627ef3dc6e828f5d37f4e191c50700c5
Arioo Barzan <arioobarzan@hotmail.com>
2024-04-14 17:16:34 +0330

    up

---
+++ Work_With_Files/Work_With_Files/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Work_With_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\gheir\Downloads\s24.txt";
+            if (!File.Exists(path))
+            {
+                var f = File.Create(path);
+                f.Close();
+            }
+            string str = "salam\nkhobi?";
+            File.WriteAllText(path, str);
+
+            File.AppendAllText(path, "\nchetori?");
+            //File.Move(path, "s24.exe");
+            var d = File.GetCreationTime(path);
+            Console.WriteLine(d);
+            Console.ReadKey();
+        }
+    }
+}
