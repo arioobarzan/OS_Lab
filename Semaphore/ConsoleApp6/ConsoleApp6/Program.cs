938e1f69bee9e3cca5952521744581b4f29e324a
Arioo Barzan <arioobarzan@hotmail.com>
2024-05-11 15:14:10 +0330

    Create Program.cs

---
+++ Semaphore/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static Semaphore semaphore = new Semaphore(2, 2); // ظرفیت Semaphore: 2
+
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Thread thread = new Thread(WithdrawMoney);
+            thread.Start(i + 1);
+        }
+        Console.ReadKey();
+    }
+
+    static void WithdrawMoney(object accountId)
+    {
+        semaphore.WaitOne(); 
+        Console.WriteLine($"Account {accountId} is withdrawing money...");
+        Thread.Sleep(2000); 
+        Console.WriteLine($"Account {accountId} has withdrawn money.");
+        semaphore.Release(); 
+    }
+}
